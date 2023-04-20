0b7917dab5f7676e0e652db4f0be8ecd05e2af15
srinivasreddych <srinivasreddych@outlook.com>
2023-04-20 13:17:34 -0500

    refactored networking module to create isolated subnets

--- modules/optionals/networking/app.py
+++ modules/optionals/networking/app.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import aws_cdk
@@ -7,47 +8,20 @@ from stack import NetworkingStack
 
 deployment_name = os.getenv("ADDF_DEPLOYMENT_NAME")
 module_name = os.getenv("ADDF_MODULE_NAME")
+internet_accessible = json.loads(os.getenv("ADDF_PARAMETER_INTERNET_ACCESSIBLE", "true"))
 
 app = App()
 
 stack = NetworkingStack(
     scope=app,
     id=f"addf-{deployment_name}-{module_name}",
+    deployment_name=deployment_name,
+    module_name=module_name,
+    internet_accessible=internet_accessible,
     env=aws_cdk.Environment(
         account=os.environ["CDK_DEFAULT_ACCOUNT"],
         region=os.environ["CDK_DEFAULT_REGION"],
     ),
-)
-
-CfnOutput(
-    scope=stack,
-    id="vpcId",
-    value=stack.vpc.vpc_id,
-)
-
-CfnOutput(
-    scope=stack,
-    id="publicSubnetIds",
-    value=",".join(stack.public_subnets.subnet_ids),
-)
-
-CfnOutput(
-    scope=stack,
-    id="privateSubnetIds",
-    value=",".join(stack.private_subnets.subnet_ids),
-)
-
-if not stack.internet_accessible:
-    CfnOutput(
-        scope=stack,
-        id="isolatedSubnetIds",
-        value=",".join(stack.isolated_subnets.subnet_ids),
-    )
-
-CfnOutput(
-    scope=stack,
-    id="nodeSubnetIds",
-    value=",".join(stack.nodes_subnets.subnet_ids),
 )
 
 CfnOutput(
